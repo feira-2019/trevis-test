aaa5a23d1d82550405564b6399128306cab3d9b4
Marcelo Lourencini Filho <marcelo.f@samsung.com>
2019-10-17 11:29:30 -0400

    baskara

--- test/calculate-test.js
+++ test/calculate-test.js
@@ -17,11 +17,18 @@ describe('Simple calc test:', () => {
 
 describe('Baskara calc test:', () => {
     const baskaraTestData = [{
-        a: 1,
-        b: -4,
-        c: -5,
-        r: [-1, 5]
-    }];
+            a: 1,
+            b: -4,
+            c: -5,
+            r: [-1, 5]
+        },
+        {
+            a: 1,
+            b: 0,
+            c: -16,
+            r: [-4, 4]
+        }
+    ];
     for (let i = 0; i < baskaraTestData.length; i++) {
         it(`a = ${baskaraTestData[i].a}, b = ${baskaraTestData[i].b}, c = ${baskaraTestData[i].c} == [${baskaraTestData[i].r[0]}, ${baskaraTestData[i].r[1]}]`, () => {
             const baskaraRoots = calculate.baskara(baskaraTestData[i].a, baskaraTestData[i].b, baskaraTestData[i].c);
